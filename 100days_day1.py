a97a0a728cdafb749dcc88870d7d5aade4370d0a
Arnold Beeka <arnoldbeeka@gmail.com>
2024-04-08 23:52:51 +0300

    day1

---
+++ 100days_day1.py
@@ -0,0 +1,28 @@
+# Band name generator(involves printing, string manipulation, commenting and variables)
+
+# print("Welcome to the Band Name Generator!")
+# city = input("What's the name of the city you grew up in?\n")
+# pet = input("What's your pet's name?\n")
+# print(f"Your band name is {city} {pet}")
+
+# Interactive exercise
+
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# Input functions
+
+# name = input("What is your name?\n")
+# print(len(name))
+
+# Variables
+
+# a = input("a: ")
+# b = input("b: ")
+# c = a
+# a = b
+# b=c
+# print(a)
+# print(b)
+
