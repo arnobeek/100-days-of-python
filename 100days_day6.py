f58aaa71d77d72644aa9bdcfdcfbd8b84ee52509
Arnold Beeka <arnoldbeeka@gmail.com>
2024-04-14 23:34:05 +0300

    'day 6'

---
+++ 100days_day6.py
@@ -0,0 +1,11 @@
+             ## Functions, Code Blocks and While Loops ##
+
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+
+# my_function()    
+
+
+while 6 == 6:
+    print("Python")
